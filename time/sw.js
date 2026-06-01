79630eea1e83813db8f4239860b17541a2d88877
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-06-01 10:28:48 -0300

    Deploy staging appearance theme setting

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-20260601131859';
+const CACHE_NAME = 'time-clock-runtime-20260601132814';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
