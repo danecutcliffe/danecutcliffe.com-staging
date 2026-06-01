8a6a912b74aad72eb4dd25a233f26a7a047ee3c5
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-06-01 14:00:44 -0300

    Deploy collapsible settings sections

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-20260601163536';
+const CACHE_NAME = 'time-clock-runtime-20260601165958';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
