d3f887936adcd467042d1277d79aab5cf713c5a2
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-06-01 10:19:44 -0300

    Deploy staging report focus styling fix

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-20260601114125';
+const CACHE_NAME = 'time-clock-runtime-20260601131859';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
