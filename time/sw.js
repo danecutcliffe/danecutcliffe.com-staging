ac295aaf9b226e0d7d2daad616f25298ad8588d1
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-06-01 08:42:22 -0300

    Deploy staging mobile nav and modal hotfix

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-20260530173032';
+const CACHE_NAME = 'time-clock-runtime-20260601114125';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
