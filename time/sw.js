2f694ea0e0c2e27addbca074670cda3bbe3d88f1
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-06-04 12:54:32 -0300

    Deploy modal scroll fix to staging

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-20260604154625';
+const CACHE_NAME = 'time-clock-runtime-20260604155326';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
