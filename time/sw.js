203d5bd48b94e82736f5ee1b20cb50d4764c46d5
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-06-04 13:51:36 -0300

    Deploy mock guardrail parity to staging

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-20260604164303';
+const CACHE_NAME = 'time-clock-runtime-20260604165129';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
