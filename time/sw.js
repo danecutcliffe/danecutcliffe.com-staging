9d25d59e24f7231b593b9d4b5af924ec9ddad13e
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-06-04 12:15:26 -0300

    Deploy employee punch guardrails to staging

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-20260604134636';
+const CACHE_NAME = 'time-clock-runtime-20260604151423';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
