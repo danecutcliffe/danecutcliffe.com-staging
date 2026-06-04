fe95cf5ab30ad2ea15e158734971e781c483e628
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-06-04 10:13:18 -0300

    Deploy QBO export cleanup to staging

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-20260604130015';
+const CACHE_NAME = 'time-clock-runtime-20260604131234';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
