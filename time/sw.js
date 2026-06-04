c038280010afdd9ccd1ef024482f1ac2f67e64be
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-06-04 14:10:05 -0300

    Deploy report readiness UI to staging

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-20260604170050';
+const CACHE_NAME = 'time-clock-runtime-20260604170950';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
