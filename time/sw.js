3fce185107eefbcaab686596817c24051ca014a7
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-06-04 13:43:11 -0300

    Deploy CSV report model fix to staging

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-20260604162759';
+const CACHE_NAME = 'time-clock-runtime-20260604164303';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
