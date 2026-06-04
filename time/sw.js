2fca61e22ed9c27f57f764c323043e948a669e37
Dane Cutcliffe <Dane@Danes-Macbook-Pro.local>
2026-06-04 14:01:05 -0300

    Deploy admin write guardrails to staging

--- time/sw.js
+++ time/sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'time-clock-runtime-20260604165129';
+const CACHE_NAME = 'time-clock-runtime-20260604170050';
 const STATIC_ASSETS = ['./manifest.json', './icon.svg'];
 
 self.addEventListener('install', (event) => {
